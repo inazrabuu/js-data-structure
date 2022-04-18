0605d77e34611c42553136f831657a7f931773e6
Arman Rizal <inazrabuu@gmail.com>
2022-04-18 09:45:44 +0700

    created: spiral matrix

---
+++ spiral-matrix.js
@@ -0,0 +1,39 @@
+const spiralOrder = function(matrix) {
+  const res = []
+  let left = 0,
+      top = 0
+      right = matrix[0].length,
+      bottom = matrix.length
+
+  while (left < right && top < bottom) {
+    for (let i = left; i < right; i++) {
+      res.push(matrix[top][i])
+    }
+    top++
+
+    for (let i = top; i < bottom; i++) {
+      res.push(matrix[i][right - 1])
+    }
+    right--
+
+    if (!(left < right && top < bottom)) {
+      break
+    }
+
+    for (let i = right - 1; i >= left; i--) {
+      res.push(matrix[bottom - 1][i])
+    }
+    bottom--
+
+    for (let i = bottom - 1; i >= top; i--) {
+      res.push(matrix[i][left])
+    }
+    left++
+  }
+
+  return res
+}
+
+let matrix = [[1,2,3],[4,5,6],[7,8,9]]
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+console.log(spiralOrder(matrix))
